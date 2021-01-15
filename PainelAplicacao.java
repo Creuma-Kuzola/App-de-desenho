0bc27e54f293d7783fe29972a4f0117bafa76b13
creuma <creumakuzola@gmail.com>
2021-01-15 21:54:00 +0100

    Adding  novo ficheiro
    Adicionando o ficheiro PainelJava

---
+++ PainelAplicacao.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package applicacao.de.desenho;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author creuma
+ */
+public class PainelAplicacao extends JPanel {
+    
+    
+    
+}
