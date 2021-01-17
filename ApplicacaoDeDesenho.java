24789a15dc4197183ce18a79e6b58df429ef5239
creuma <creumakuzola@gmail.com>
2021-01-17 16:26:18 +0100

    Atualizações
    Atualizei a Main classe da Aplicação.
    Removi todo o código desnecessário e deixei apenas a instanciação da classe FramePrincipal!

--- ApplicacaoDeDesenho.java
+++ ApplicacaoDeDesenho.java
@@ -3,13 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package applicacao.de.desenho;
 
-import java.awt.Dimension;
-import java.awt.Toolkit;
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
+
+import applicacao.de.desenho.FramePrincipal;
 
 /**
  *
@@ -18,55 +14,15 @@ import javax.swing.JMenuBar;
 public class ApplicacaoDeDesenho {
 
     /**
-     * @return 
+     * @param args 
      */
-    
-    public static int larguraDimensaoDaTela(){
+   
         
-        Toolkit kit = Toolkit.getDefaultToolkit();
-        Dimension tamanhoTela = kit.getScreenSize();   
-        return tamanhoTela.width;   
-    }
-    
-    public static int alturaDimensaoDaTela(){
-        
-        Toolkit kit = Toolkit.getDefaultToolkit();
-        Dimension tamanhoTela = kit.getScreenSize();
-        return tamanhoTela.height;
-     
-    }
-
-    
-    public static void criarMenuBar()
-    { 
-    
-        JFrame frame = new JFrame("Aplicação de desenho");
-        //PainelAplicacao painel = new PainelAplicacao();
-        frame.setSize(larguraDimensaoDaTela()/2, alturaDimensaoDaTela()/2);
-        //frame.setSize(500,500);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        
-        JMenuBar menuBar = new JMenuBar();
-        frame.setJMenuBar(menuBar);
-        
-        JMenu file = new JMenu("File");
-        menuBar.add(file);
-        
-        JMenu cvcv = new JMenu("Bla bla");
-        menuBar.add(cvcv);
-      
-        //frame.add(painel);
-
-    
-    }        
-    
     public static void main(String[] args) {
         // TODO code application logic here
         
-        criarMenuBar();
-       
+       new FramePrincipal();
+        
     }
     
 }
